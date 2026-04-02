e7b756df8399a1b46b279d0e6d3697f2f6a34f43
Morgan T. Sterling <sunskebateback@gmx.net>
2026-04-02 09:25:30 -0400

    Update detector.hpp with high-quality code

--- detector.hpp
+++ detector.hpp
@@ -1,4 +1,14 @@
+#ifndef DETECTOR_HPP
+#define DETECTOR_HPP
+
+#include <opencv2/opencv.hpp>
+
 class ObjectDetector {
 public:
-    void detect();
+    ObjectDetector(const std::string& modelPath);
+    void detect(const cv::Mat& frame, std::vector<cv::Rect>& detections);
+private:
+    cv::dnn::Net net;
 };
+
+#endif // DETECTOR_HPP
